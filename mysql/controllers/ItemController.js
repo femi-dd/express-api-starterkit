487a17367b0a3fedf3726728a4268a15a1dc3d75
Femi_DD <femi.highsky@gmail.com>
2019-02-13 10:38:21 -0600

    Completed crud operations with mysqldb
    - Added create operation
    - Added read operation

--- mysql/controllers/ItemController.js
+++ mysql/controllers/ItemController.js
@@ -1,6 +1,73 @@
 const mysqlx = require('@mysql/xdevapi');
 const db_config = require('../config/db');
 
+// Create new item
+exports.createNewItem = (req, res) => {
+   const { name, color, price } = { ...req.body };
+   mysqlx
+      .getSession(db_config)
+      .then(session => session.getSchema(db_config.schema).getTable('items'))
+      .then(table => {
+         return table
+            .insert(['name', 'color', 'price'])
+            .values(name, color, price)
+            .execute((item) => item)
+      }).then(result => {
+         if (result.getAffectedItemsCount() == 0) {
+            res.status(500).json({
+               msg: 'An error occured'
+            });
+         }
+
+         if (result.getAffectedItemsCount() == 1) {
+            res.status(201).json({
+               msg: 'Item successfully added',
+               data: {
+                  item: req.body
+               }
+            });
+         }
+      })
+      .catch(error => {
+         res.status(500).json({
+            msg: 'An error occured',
+            error: error.info.msg
+         });
+      });
+}
+
+// Read all items
+exports.getAllItems = (req, res) => {
+   let results = [];
+   mysqlx
+      .getSession(db_config)
+      .then(session => session.getSchema(db_config.schema).getTable('items'))
+      .then(items => items.select().execute(row => results.push(row)))
+      .then(() => {
+         if (!results) {
+            res.status(500).json({
+               msg: 'An error occured'
+            });
+         }
+
+         if (results) {
+            res.status(200).json({
+               msg: 'All items',
+               data: {
+                  items: results
+               }
+            });
+         }
+      })
+      .catch(error => {
+         res.status(500).json({
+            msg: 'An error occured',
+            error: error.info.msg
+         });
+      });
+}
+
+// Update an item
 exports.updateItemById = (req, res) => {
    const { id, name, color, price } = { ...req.body };
    mysqlx
@@ -30,15 +97,16 @@ exports.updateItemById = (req, res) => {
                }
             });
          }
-      }).catch(error => {
-         console.log(error);
+      })
+      .catch(error => {
          res.status(500).json({
             msg: 'An error occured',
             error: error.info.msg
          });
-      })
+      });
 }
 
+// Delete an existing item
 exports.deleteItemById = (req, res) => {
    const id = req.params.id;
    mysqlx
@@ -63,6 +131,7 @@ exports.deleteItemById = (req, res) => {
       .catch(error => {
          res.status(500).json({
             msg: 'An error occured',
-         })
-      })
+            error: error.info.msg
+         });
+      });
 }
