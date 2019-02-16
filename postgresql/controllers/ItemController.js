752f2f6bd6516c0dd769b78119d2ca7b1646ecd7
Femi_DD <femi.highsky@gmail.com>
2019-02-15 19:09:08 -0600

    Added model for postgresql

--- postgresql/controllers/ItemController.js
+++ postgresql/controllers/ItemController.js
@@ -1,65 +1,49 @@
-const { Pool } = require('pg');
-const dbConfig = require('../config/db');
+const ItemModel = require('../models/ItemModel');
 
 // Create new item
 exports.createNewItem = (req, res) => {
   const item = req.body;
-  const query = {
-    text: 'insert into items (name, color, price) values ($1, $2, $3)',
-    values: [item.name, item.color, item.price],
-  };
-  const pool = new Pool(dbConfig);
 
-  pool.query(query, (error, result) => {
-    if (error) {
-      res.status(500).json({
-        msg: 'An error occured',
-        err: error.message,
-      });
-    } else {
-      res.status(201).json({
-        msg: 'Item successfully created.',
-        data: {
-          item,
-        },
-      });
-    }
-  });
-
-  pool.end();
+  ItemModel.AddNewItem(item, res)
+    .then((error, result) => {
+      if (error) {
+        res.status(500).json({
+          msg: 'An error occured',
+          err: error,
+        });
+      } else {
+        res.status(201).json({
+          msg: 'Item successfully created.',
+          data: {
+            item,
+          },
+        });
+      }
+    });
 };
 
 // Read all items
 exports.getAllItems = (req, res) => {
-  const query = 'select * from items';
-  const pool = new Pool(dbConfig);
-
-  pool.query(query, (error, result) => {
-    if (error) {
-      res.status(500).json({
-        msg: 'An error occured',
-        err: error.message,
-      });
-    } else {
+  ItemModel.getAllItems()
+    .then((result) => {
       res.status(200).json({
         msg: 'Success',
         data: {
           items: result.rows,
         },
       });
-    }
-  });
-
-  pool.end();
+    })
+    .catch((error) => {
+      res.status(500).json({
+        msg: 'An error occured',
+        err: error.message,
+      });
+    });
 };
 
 // Update an item
 exports.updateItemById = (req, res) => {
   const item = req.body;
-  const query = {
-    text: 'update items set name = $1, color = $2, price = $3 where id = $4',
-    values: [item.name, item.color, item.price, item.id],
-  };
 
   if (req.params.id !== item.id) {
     res.status(400).json({
@@ -68,49 +52,38 @@ exports.updateItemById = (req, res) => {
     return;
   }
 
-  const pool = new Pool(dbConfig);
-
-  pool.query(query, (error, result) => {
-    if (error) {
-      res.status(500).json({
-        msg: 'An error occured',
-        err: error.message,
-      });
-    } else {
+  ItemModel.updateItem(item)
+    .then((result) => {
       res.status(200).json({
         msg: 'Item successfully updated',
         data: {
           item,
         },
       });
-    }
-  });
-
-  pool.end();
+    })
+    .catch((error) => {
+      res.status(500).json({
+        msg: 'An error occured',
+        err: error.message,
+      });
+    });
 };
 
 // Delete an existing item
 exports.deleteItemById = (req, res) => {
   const { id } = req.params;
-  const query = {
-    text: 'delete from items where id = $1',
-    values: [id],
-  };
-  const pool = new Pool(dbConfig);
 
-  pool.query(query, (error, result) => {
-    if (error) {
+  ItemModel.deleteItem(id)
+    .then((result) => {
+      res.status(200).json({
+        msg: 'Item successfully deleted',
+        data: id,
+      });
+    })
+    .catch((error) => {
       res.status(500).json({
         msg: 'An error occured',
         err: error.message,
       });
-    } else {
-      res.status(200).json({
-        msg: 'Success',
-        data: id,
-      });
-    }
-  });
-
-  pool.end();
+    });
 };
