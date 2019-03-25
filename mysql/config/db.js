7cecfb5f7502e66c8f28217f0da1c0aaa7363cf0
Femi_DD <femi.highsky@gmail.com>
2019-03-25 12:38:35 -0500

    CleanUp master branch

--- mysql/config/db.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  password: 'password123456',
-  user: 'root',
-  host: 'localhost',
-  port: 33060,
-  schema: 'express-starterkit',
-};
--- postgresql/config/db.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  user: 'postgres',
-  host: 'localhost',
-  database: 'express-starterkit',
-  password: 'root',
-  port: '5432',
-};
